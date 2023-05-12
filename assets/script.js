84460ad5ad9abe88cb0ae2ec030283b5218cd340
James Horgan <jamesrhorgan@gmail.com>
2023-05-12 13:16:26 +0000

    Add rules modal

--- assets/script.js
+++ assets/script.js
@@ -271,3 +271,29 @@ let questions = [
 // function for time of day 
 
 // function to initiate launch sequence 
+
+// Get the modal
+let rulesModal = document.getElementById("rules-modal");
+
+// Get the button that opens the modal
+let rulesButton = document.getElementById("rules-button");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close-rules-modal")[0];
+
+// When the user clicks on the button, open the modal
+rulesButton.onclick = function() {
+  rulesModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  rulesModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    rulesModal.style.display = "none";
+  }
+}
