5448351475cd86a0956116a7840b19afa5b75871
James Horgan <jamesrhorgan@gmail.com>
2023-05-15 19:12:40 +0000

    Split questions into own file

---
+++ assets/js/questions.js
@@ -0,0 +1,163 @@
+// setting a list of questions and initialising it to an array
+const questions = [{
+        question: 'Who was the first animal to orbit the Earth',
+        answers: [{
+                text: 'Laika the Dog',
+                correct: true
+            },
+            {
+                text: 'Edward the Elephant',
+                correct: false
+            },
+            {
+                text: 'Bernard the Monkey',
+                correct: false
+            },
+            {
+                text: 'Ham the Chimp',
+                correct: false
+            },
+        ]
+    },
+    {
+        question: 'Who was the first man to walk on the moon?',
+        answers: [{
+                text: 'Michael Jackson',
+                correct: false
+            },
+            {
+                text: 'Buzz Lightyear',
+                correct: false
+            },
+            {
+                text: 'Neil Armstrong',
+                correct: true
+            },
+            {
+                text: 'Michael Collins',
+                correct: false
+            },
+        ]
+    },
+    {
+        question: 'What does the "ISS" stand for?',
+        answers: [{
+                text: 'Instant Salami Sandwich',
+                correct: false
+            },
+            {
+                text: 'Ionic Space Shield',
+                correct: false
+            },
+            {
+                text: 'Incredible Space Suit',
+                correct: false
+            },
+            {
+                text: 'International Space Station',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'How far away is the Sun?',
+        answers: [{
+                text: '200 Thousand Miles',
+                correct: false
+            },
+            {
+                text: '93 Million Miles',
+                correct: true
+            },
+            {
+                text: '88 Billion Miles',
+                correct: false
+            },
+            {
+                text: 'Quadrillion Miles',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'Instead of water, what rains on Jupiter?',
+        answers: [{
+                text: 'Acid',
+                correct: false
+            },
+            {
+                text: 'Iron',
+                correct: false
+            },
+            {
+                text: 'Fire',
+                correct: false
+            },
+            {
+                text: 'Diamonds',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'How long does it take for people to go to the moon?',
+        answers: [{
+                text: '3 days',
+                correct: true
+            },
+            {
+                text: '2 weeks',
+                correct: false
+            },
+            {
+                text: '5 hours',
+                correct: false
+            },
+            {
+                text: '6 days',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What is the smallest planet in the Solar System?',
+        answers: [{
+                text: 'Venus',
+                correct: false
+            },
+            {
+                text: 'Saturn',
+                correct: false
+            },
+            {
+                text: 'Neptune',
+                correct: false
+            },
+            {
+                text: 'Mercury',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'In what galaxy is the Earth in?',
+        answers: [{
+                text: 'The Solar Galaxy',
+                correct: false
+            },
+            {
+                text: 'The Milky Way',
+                correct: true
+            },
+            {
+                text: 'The Shiny Way',
+                correct: false
+            },
+            {
+                text: 'Andromeda Galaxy',
+                correct: false
+            }
+        ]
+    },
+
+];
